19f3e30af3ae85734ca8c75c21bc8a75603133ef
FabulousDonkey777 <oceaniagb1984@gmail.com>
2025-01-15 12:59:36 +0300

    Lesson_3

---
+++ src/Task5.java
@@ -0,0 +1,8 @@
+
+public class Task5 {
+
+    public static boolean InRange(int a, int b) {
+        int sum = a + b;
+        return sum >= 10 && sum <= 20;
+    }
+}
---
+++ src/Task7.java
@@ -0,0 +1,8 @@
+public class Task7 {
+
+    public static boolean Negative(int a) {
+        return a < 0;
+    }
+
+
+}
