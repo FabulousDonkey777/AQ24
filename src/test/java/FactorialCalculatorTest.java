6c9cec5c1edda019f6640bfa8f84c7a005bf6dfb
FabulousDonkey777 <oceaniagb1984@gmail.com>
2025-02-18 18:54:12 +0300

    Lesson_13_junit5

--- src/test/java/FactorialCalculatorTest.java
+++ src/test/java/FactorialCalculatorTest.java
@@ -4,18 +4,27 @@ import static org.junit.jupiter.api.Assertions.*;
 public class FactorialCalculatorTest {
 
     @Test
-    public void  testFactorialOfZero() {
-        assertEquals(1, FactorialCalculator.calculateFactorial(0));
+    public void testCalculateFactorialZero() {
+        assertEquals(1, FactorialCalculator.calculateFactorial(0), "Факториал 0 должен быть 1.");
     }
 
     @Test
-    public void  testFactorialOfPositiveNumber() {
-        assertEquals(120, FactorialCalculator.calculateFactorial(5));
-        assertEquals(720, FactorialCalculator.calculateFactorial(6));
+    public void testCalculateFactorialOne() {
+        assertEquals(1, FactorialCalculator.calculateFactorial(1), "Факториал 1 должен быть 1.");
     }
 
     @Test
-    public void testFactorialOfNegativeNumber() {
+    public void testCalculateFactorialPositiveNumber() {
+        assertEquals(120, FactorialCalculator.calculateFactorial(5), "Факториал 5 должен быть 120.");
+    }
+
+    @Test
+    public void testCalculateFactorialLargeNumber() {
+        assertEquals(3628800, FactorialCalculator.calculateFactorial(10), "Факториал 10 должен быть 3628800.");
+    }
+
+    @Test
+    public void testCalculateFactorialNegativeNumber() {
         Exception exception = assertThrows(IllegalArgumentException.class, () -> {
             FactorialCalculator.calculateFactorial(-1);
         });
