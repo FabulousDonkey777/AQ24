0c982dc91f1b9d1322a32f8b74e86ff953192577
FabulousDonkey777 <oceaniagb1984@gmail.com>
2025-02-18 18:28:52 +0300

    Lesson_13_testng

---
+++ src/FactorialCalculatorTest.java
@@ -0,0 +1,38 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class FactorialCalculatorTest {
+
+    @Test
+    public void testFactorialOfZero() {
+        long expected = 1;
+        long actual = FactorialCalculator.calculateFactorial(0);
+        Assert.assertEquals(actual, expected, "Факториал 0 должен быть равен 1");
+    }
+
+    @Test
+    public void testFactorialOfOne() {
+        long expected = 1;
+        long actual = FactorialCalculator.calculateFactorial(1);
+        Assert.assertEquals(actual, expected, "Факториал 1 должен быть равен 1");
+    }
+
+    @Test
+    public void testFactorialOfPositiveNumber() {
+        long expected = 120;
+        long actual = FactorialCalculator.calculateFactorial(5);
+        Assert.assertEquals(actual, expected, "Факториал 5 должен быть равен 120");
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testFactorialOfNegativeNumber() {
+        FactorialCalculator.calculateFactorial(-1);
+    }
+
+    @Test
+    public void testFactorialOfLargeNumber() {
+        long expected = 2432902008176640000L;
+        long actual = FactorialCalculator.calculateFactorial(20);
+        Assert.assertEquals(actual, expected, "Факториал 20 должен быть равен 2432902008176640000");
+    }
+}
